78762d41f3f5e281d5c9700f5b1bf4d517208efe
corentinsteinhauser <costein972@gmail.com>
2022-05-05 17:31:32 +0200

    basic mouvement

--- src/main.py
+++ src/main.py
@@ -1,17 +1,70 @@
+import pygame
+
 from src.settings import *
 
 pygame.init()
 pygame.time.set_timer(pygame.USEREVENT, 1000)
 
 # création de la fenêtre
-screen = pygame.display.set_mode((800, 800))
-#pygame.display.set_caption("Water Rocket")
-
-while game_run:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      game_run = False
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Etoiles Vso")
 
 
+def main_window():
+  GAME_RUNNING = True
 
-pygame.display.flip()
+  x_pose = 50
+  y_pose = 50
+
+  inputMap = [False, False, False, False]
+
+  while GAME_RUNNING:
+
+    # EVENT
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        GAME_RUNNING = False
+      if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_w:
+          inputMap[0] = True;
+        if event.key == pygame.K_s:
+          inputMap[1] = True;
+        if event.key == pygame.K_a:
+          inputMap[2] = True;
+        if event.key == pygame.K_d:
+          inputMap[3] = True;
+      if event.type == pygame.KEYUP:
+        if event.key == pygame.K_w:
+          inputMap[0] = False;
+        if event.key == pygame.K_s:
+          inputMap[1] = False;
+        if event.key == pygame.K_a:
+          inputMap[2] = False;
+        if event.key == pygame.K_d:
+          inputMap[3] = False;
+
+    vso_speedX = 0
+    if inputMap[2]: vso_speedX -= 1.5
+    if inputMap[3]: vso_speedX += 1.5
+    vso_speedY = 0
+    if inputMap[0]: vso_speedY -= 1.5s
+    if inputMap[1]: vso_speedY += 1.5
+
+    x_pose += vso_speedX
+    y_pose += vso_speedY
+
+        # background
+    screen.fill(DARKGRAY)
+
+    #VSO tst
+    vso = pygame.Rect(x_pose, y_pose, 50, 50)
+    pygame.draw.rect(screen, RED, vso, 25)
+
+    # Flip the display
+    pygame.display.flip()
+
+
+main_window()
+
+
+#pygame.display.flip()
--- src/settings.py
+++ src/settings.py
@@ -1,12 +1,12 @@
 import pygame
 from random import *
 
-#pygame.font.init()
+pygame.font.init()
 
 # Variables
 WIDTH = 1080
 HEIGHT = 720
-game_run = True
+GAME_RUNNING = True
 click = False
 clock = pygame.time.Clock()
 
@@ -21,3 +21,26 @@ DARKGRAY = (40, 40, 40)
 PURPLE = (120, 0, 120)
 COLOR_INACTIVE = pygame.Color('lightskyblue3')
 COLOR_ACTIVE = pygame.Color('dodgerblue2')
+
+# fonts
+police = pygame.font.Font(None, 50)
+police_subtitle = pygame.font.Font(None, 25)
+mini_police = pygame.font.Font(None, 20)
+mega_police = pygame.font.Font(None, 150)
+FONT = pygame.font.Font(None, 32)
+
+def draw_text(text, font, color, surface, x, y):
+    # draw text on a screen
+
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+
+def Rot_center(image, angle):
+    orig_rect = image.get_rect()
+    rot_image = pygame.transform.rotate(image, angle)
+    rot_rect = orig_rect.copy()
+    rot_rect.center = rot_image.get_rect().center
+    rot_image = rot_image.subsurface(rot_rect).copy()
+    return rot_image
